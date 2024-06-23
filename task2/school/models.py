a8f9085946136e85cb51a649f453577eea502d52
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-06-24 00:43:04 +0200

    Renderd html

--- task2/school/models.py
+++ task2/school/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
-# Create your models here.
-# Key Criteria to Consider:
-# Location: Use latitude and longitude to determine proximity to the student's residence.
-# School Type: Public vs. Private (Government vs. Non-Government).
-# Year Levels: Range of years offered by the school (e.g., Y07-Y12).
-# Student Performance Metrics: ATAR Rank, Median ATAR, BE Score, ICSEA (Index of Community Socio-Educational Advantage)
+import pandas as pd
+
+class SchoolData():
+    def __init__(self):
+        self.file_path = 'schools.csv'
+        self.df = pd.read_csv(self.file_path)
+
+    def get_schools_from_csv(self):
+
+        df2 = self.df[['School Name', 'Street', 'Suburb', 'State', 'Postcode',
+            'Postal Street', 'Postal Suburb', 'Postal State', 'Postal Postcode',
+            'Latitude', 'Longitude', 'Phone', 'Education Region',
+            'Broad Classification', 'Classification Group', 'Low Year', 'High Year', 'Total Students', 'BE Score',
+            'ICSEA', 'ATAR Rank', 'Median ATAR','% students with ATAR']].copy()
+        
+        return df2
--- task2/school/views.py
+++ task2/school/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import pandas as pd
-from data import SchoolData
+from .models import SchoolData
 
 school_data = SchoolData()
 all_schools = school_data.get_schools_from_csv()
 html_table = all_schools.to_html()
 
-def pandas_example_view(request):
-    # Convert DataFrame to HTML for rendering in template
-    html_table = all_schools.to_html()
 
-    return render(request, 'myapp/pandas_example.html', {'html_table': html_table})
+def index(request):
+
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'school/table1.html', {'html_table': html_table})
 
 
 
