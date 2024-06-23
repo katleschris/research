82aa82a8b33d1ed52d066b1903927df80d73f2da
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-06-24 00:05:14 +0200

    Completed exporting data

---
+++ task2/school/data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+class SchoolData():
+    def __init__(self):
+        self.file_path = './schools.csv'
+        self.df = pd.read_csv(self.file_path)
+
+    def get_schools_from_csv(self):
+
+        df2 = self.df[['School Name', 'Street', 'Suburb', 'State', 'Postcode',
+            'Postal Street', 'Postal Suburb', 'Postal State', 'Postal Postcode',
+            'Latitude', 'Longitude', 'Phone', 'Education Region',
+            'Broad Classification', 'Classification Group', 'Low Year', 'High Year', 'Total Students', 'BE Score',
+            'ICSEA', 'ATAR Rank', 'Median ATAR','% students with ATAR']].copy()
+        
+        return df2
--- task2/school/views.py
+++ task2/school/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import pandas as pd
+from data import SchoolData
 
-# Create your views here.
+school_data = SchoolData()
+all_schools = school_data.get_schools_from_csv()
+html_table = all_schools.to_html()
+
+def pandas_example_view(request):
+    # Convert DataFrame to HTML for rendering in template
+    html_table = all_schools.to_html()
+
+    return render(request, 'myapp/pandas_example.html', {'html_table': html_table})
+
+
+
